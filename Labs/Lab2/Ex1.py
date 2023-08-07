857462d6d7cf1a0734029d88f5d7e1d5c251b17b
dieggspapu <alo20172@uvg.edu.gt>
2023-08-07 09:17:40 -0600

    update:Finale
    Ley debil de los grandes numeros, ejercicio 1 done

--- Labs/Lab2/Ex1.py
+++ Labs/Lab2/Ex1.py
@@ -1,19 +1,16 @@
-# Diego Andres Alonzo Medinilla 20172
-# Guillermo
-import numpy as np
+from scipy.stats import uniform
 import matplotlib.pyplot as plt
-sum = 0
-probs = []
-rang = []
-for x in range(0,1000000):
-    probs.append(np.random.random_sample())
-    rang.append(x)
-    sum+= probs[x]
-print("Promedio: "+str(sum))
-plt.plot(rang, probs)
-plt.xlabel('x - axis')
-plt.ylabel('y - axis')
-plt.title('Ley debil de los grandes numeros')
+import numpy as np
+# Generate an array of 1*10^6 random variates from the uniform distribution [0, 1]
+y = uniform.rvs(loc=0, scale=1  , size=1000000)
+# Calculate the partial arithmetic mean of each one of the values, it means it will do an acumulative sum. Acumulative sum of probability divided by a million to get the partial.
+y = np.cumsum(y)/np.arange(1, 1000000 + 1)
+# Graph Partial Mean
+plt.plot(np.arange(1, 1000000 + 1), y, label = "Medias Parciales" )
+# Graph Real mean
+plt.plot(np.arange(1, 1000000 + 1), np.full(1000000,0.5), label ="Media Real")
+plt.xlabel("Muestra (n)")
+plt.ylabel("Media")
+plt.title("Ley debil de los grandes numeros")
+plt.legend()
 plt.show()
-# for x in probs:
-#     print(x)
