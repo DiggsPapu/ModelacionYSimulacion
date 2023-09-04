a9998f590031213fc898d117db1edb55e79285e7
KennethGalvez <60400757+KennethGalvez@users.noreply.github.com>
2023-09-04 16:57:34 -0600

    Update Ej2.py

--- Ej2.py
+++ Ej2.py
@@ -1,13 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Función para generar variables aleatorias de una distribución discreta, escogemos Poisson
+# Función para generar variables aleatorias de una distribución discreta (Poisson) utilizando transformada inversa
 def generate_discrete_random_variable(lambda_value, n):
-    return np.random.poisson(lambda_value, n)
+    u = np.random.uniform(0, 1, n)
+    poisson_values = -np.log(u) / lambda_value
+    return np.floor(poisson_values).astype(int)
 
-# Función para generar variables aleatorias de una distribución continua, esogemos Exponencial
+# Función para generar variables aleatorias de una distribución continua (Exponencial) utilizando aceptación y rechazo
 def generate_continuous_random_variable(scale, n):
-    return np.random.exponential(scale, n)
+    result = []
+    while len(result) < n:
+        x = np.random.exponential(scale)
+        u = np.random.uniform(0, 1)
+        if u < np.exp(-x / scale):
+            result.append(x)
+    return np.array(result)
 
 # Función para calcular las medias aritméticas parciales
 def calculate_partial_means(data):
@@ -17,8 +25,8 @@ def calculate_partial_means(data):
 
 # Configuración de parámetros
 n_simulations = 1000000  # Número de simulaciones
-lambda_value = 3.0  # Parámetro lambda para Poisson
-scale_value = 2.0  # Parámetro de escala para Exponencial
+lambda_value = 3.0  # Parámetro lambda para la distribución Poisson
+scale_value = 2.0  # Parámetro de escala para la distribución Exponencial
 
 # Generar variables aleatorias y calcular las medias parciales
 discrete_data = generate_discrete_random_variable(lambda_value, n_simulations)
@@ -30,6 +38,7 @@ partial_means_continuous = calculate_partial_means(continuous_data)
 # Calcular la media verdadera para comparar
 true_mean_discrete = np.mean(discrete_data)
 true_mean_continuous = np.mean(continuous_data)
+
 
 # Graficar los resultados
 plt.figure(figsize=(12, 6))
