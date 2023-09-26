1060d81ff41447f7eca29360dbd73c97afb4991b
diggsy <alo20172@uvg.edu.gt>
2023-09-26 12:33:27 -0600

    update:Ex3
    Maximizacion algoritmo terminado falta el dual

--- Ej3.py
+++ Ej3.py
@@ -1,66 +1,52 @@
-import numpy as np
-from scipy.optimize import linprog
+from pulp import *
+
+def maximizar(variables, funcion_objetivo_coeficientes, restricciones_coeficientes, valores_restricciones):
+    # Definir el problema
+    problema = LpProblem("Problema_Primal", LpMaximize)
+    # Crear variables
+    var_dict = {var: LpVariable(var, lowBound=0) for var in variables}
+    # Definir la función objetivo
+    funcion_objetivo = sum(coef * var_dict[var] for coef, var in zip(funcion_objetivo_coeficientes, variables))
+    problema += funcion_objetivo
+    # Crear restricciones
+    for i, coeficientes in enumerate(restricciones_coeficientes):
+        restriccion = sum(coef * var_dict[var] for coef, var in zip(coeficientes, variables))
+        operador = "<=" if valores_restricciones[i] >= 0 else ">="  # Cambiar a ">=" si el valor es negativo
+        problema += restriccion <= valores_restricciones[i], f'Restriccion_{i} ({operador} {valores_restricciones[i]})'
+    # Mostrar el problema
+    print(problema)
+    # Resolver el problema
+    solution = problema.solve()
+    # Generar el optimo
+    assert solution == LpStatusOptimal
+    # Imprimir los valores optimos
+    for var in var_dict:
+        print('Valores optimos {}={}'.format(var_dict[var].name, var_dict[var].value()))    
+    # Devolver el valor óptimo de la maximización
+    print("Maximizacion: "+str(value(problema.objective)))
+
+# Ejercicio 3.a
+variables = ['x1', 'x2', 'x3']
+funcion_objetivo_coeficientes = [1, -1, 1]
+restricciones_coeficientes = [
+    [1, 1, 2],
+    [2, 1, 1],
+    [2, -1, 3],
+    [1, 2, 5]
+]
+valores_restricciones = [5, 7, 8, 9]
+maximizar(variables, funcion_objetivo_coeficientes, restricciones_coeficientes, valores_restricciones)
 
 
-def problemaMaximizacion(funcionObjetivo=[1,1,1], 
-                         restriccionesIzquierdo =[
-                        [1,1,1],
-                        [1,1,1],
-                        [1,1,1]
-                        ],
-                        restriccionesDerecho=[1,1,1],
-                        limites=[0,0,0]):
-    
-    # Coeficientes de la función objetivo
-    c = np.array(funcionObjetivo)
-    # Coeficientes de las restricciones (matriz A), de manera que da las ecuaciones de forma: x1+x2+x3<=
-    A = np.array(restriccionesIzquierdo)
-    # Lados derecho de las restricciones son la otra parte de la desigualdad es el <=n
-    b = np.array(restriccionesDerecho)
-    # Definir los límites de las variables (x >= 0, y >= 0, z>=0)
-    limites = [(i,None) for i in limites]
-    # Resolver el problema primal
-    result_primal = linprog(c, A_ub=A, b_ub=b, bounds=limites, method='highs')
-    # Imprimir resultados
-    print("Problema Primal maximizar x1-x2+x3:")
-    print("Status:", result_primal.message)
-    print("x1 =", result_primal.x[0])
-    print("x2 =", result_primal.x[1])
-    print("x3 =", result_primal.x[2])
-    print("Valor óptimo =", -result_primal.fun)
-    
-c = np.array([1,-1,1])
+# Ejercicio 3.b
+variables = ['x1', 'x2', 'x3', 'x4']
+funcion_objetivo_coeficientes = [5, 7, 15, 6]
+restricciones_coeficientes = [
+    [1, 2, 0, 1],
+    [1, 3, 1, 0],
+    [1, 4, 3, 2],
+    [1, 0, 5, 3]
+]
+valores_restricciones = [1, 2, 3, 4]
+maximizar(variables, funcion_objetivo_coeficientes, restricciones_coeficientes, valores_restricciones)
 
-
-A = np.array([
-    [1,1,2],
-    [2,1,1],
-    [2,-1,3],
-    [1,2,5]
-    
-])
-
-
-b = np.array([5,7,8,9])
-
-x_bounds = (0, None)
-y_bounds = (0, None)
-z_bounds = (0, None)
-
-# Resolver el problema primal
-result_primal = linprog(c, A_ub=A, b_ub=b, bounds=[x_bounds, y_bounds, z_bounds], method='highs')
-
-# Imprimir resultados
-print("Problema Primal maximizar x1-x2+x3:")
-print("Status:", result_primal.message)
-print("x1 =", result_primal.x[0])
-print("x2 =", result_primal.x[1])
-print("x3 =", result_primal.x[2])
-print("Valor óptimo =", -result_primal.fun)
-
-problemaMaximizacion([1,-1,1],[
-    [1,1,2],
-    [2,1,1],
-    [2,-1,3],
-    [1,2,5]
-    ],[5,7,8,9],[0,0,0])
