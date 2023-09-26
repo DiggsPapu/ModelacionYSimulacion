0dbc53d7b36f32990e658f117b2b39e78941555b
KennethGalvez <60400757+KennethGalvez@users.noreply.github.com>
2023-09-26 17:04:03 -0600

    Update Ej2.py

--- Ej2.py
+++ Ej2.py
@@ -26,6 +26,16 @@ def rk4(f, x0, y0, h, n):
     
     return x_values, y_values
 
+def plot_trajectory(x_values, y_values, title):
+    plt.figure()
+    plt.plot(x_values, y_values, label='Trayectoria', color='blue')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title(title)
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
 # Inciso a)
 def f_a(x, y):
     dxdt = -x + y
@@ -37,11 +47,13 @@ h_a = 0.1
 n_a = 100
 
 x_values_a, y_values_a = rk4(f_a, x0_a, y0_a, h_a, n_a)
+plot_trajectory(x_values_a, y_values_a, 'Trayectoria para el Sistema a)')
+
 
 # Imprimir los valores de x y y para el inciso a)
 print("Inciso a)")
-for i, (x, y) in enumerate(zip(x_values_a, y_values_a)):
-    print(f"n={i}, x={x}, y={y}")
+for x, y in zip(x_values_a, y_values_a):
+    print(f"x={x}, y={y}")
 
 
 # Campo vectorial para el inciso a)
@@ -68,11 +80,13 @@ h_b = 0.1
 n_b = 100
 
 x_values_b, y_values_b = rk4(f_b, x0_b, y0_b, h_b, n_b)
+plot_trajectory(x_values_b, y_values_b, 'Trayectoria para el Sistema b)')
+
 
 # Imprimir los valores de x y y para el inciso b)
 print("\nInciso b)")
-for i, (x, y) in enumerate(zip(x_values_b, y_values_b)):
-    print(f"n={i}, x={x}, y={y}")
+for x, y in zip(x_values_b, y_values_b):
+    print(f"x={x}, y={y}")
 
 
 # Campo vectorial para el inciso b)
@@ -96,12 +110,12 @@ h_c = 0.1
 n_c = 100
 
 x_values_c, y_values_c = rk4(f_c, x0_c, y0_c, h_c, n_c)
+plot_trajectory(x_values_c, y_values_c, 'Trayectoria para el Sistema c)')
 
 # Imprimir los valores de x y y para el inciso c)
 print("\nInciso c)")
-for i, (x, y) in enumerate(zip(x_values_c, y_values_c)):
-    print(f"n={i}, x={x}, y={y}")
-
+for x, y in zip(x_values_c, y_values_c):
+    print(f"x={x}, y={y}")
 
 # Campo vectorial para el inciso c)
 dx, dy = f_c(X, Y)
