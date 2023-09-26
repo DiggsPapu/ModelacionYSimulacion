d2fa238387cc54f13e8d6d5fd99f9c1673c91e2c
Guillermo Santos <guillermo.santosba@gmail.com>
2023-09-26 14:42:50 -0600

    Arreglando ejercicio 1

--- Ej1.py
+++ Ej1.py
@@ -18,22 +18,32 @@ def select_parents(population, fitness_scores):
 
 # Función de cruce (cruce en un punto)
 def crossover(parent1, parent2):
-		child1 = (parent1[0], parent2[1])
-		child2 = (parent2[0], parent1[1])
+		crossover_point = random.randint(0, 1)
+		child1 = (parent1[0], parent2[crossover_point])
+		child2 = (parent2[0], parent1[crossover_point])
 		return child1, child2
 
 # Función de mutación (mutación por cambio aleatorio)
-def mutate(individual, mutation_prob):
+def mutate(individual, mutation_prob, algorithm = 'a'):
+		if algorithm == 'a':
+			upper_x1 = 45			
+			upper_x2 = 22
+		elif algorithm == 'b':
+			upper_x1 = 1
+			upper_x2 = 9
+		elif algorithm == 'c':
+			upper_x1 = 2
+			upper_x2 = 2
 		if random.random() < mutation_prob:
 				index = random.randint(0, 1)
 				if index == 0:
-						individual = (random.uniform(0, 15), individual[1])
+						individual = (random.uniform(0, upper_x1), individual[1])
 				else:
-						individual = (individual[0], random.uniform(0, 15))
+						individual = (individual[0], random.uniform(0, upper_x2))
 		return individual
 
 
-def genetic_algorithm(population_size, num_generations, mutation_probability, is_valid, fitness_function):
+def genetic_algorithm(population_size, num_generations, mutation_probability, is_valid, fitness_function, algorithm = 'a'):
 	# Inicializar la población inicial
 	population = [initialize_individual() for _ in range(population_size)]
 	
@@ -48,15 +58,20 @@ def genetic_algorithm(population_size, num_generations, mutation_probability, is
 					parent1 = select_parents(valid_population, fitness_scores)
 					parent2 = select_parents(valid_population, fitness_scores)
 					child1, child2 = crossover(parent1, parent2)
-					child1 = mutate(child1, mutation_probability)
-					child2 = mutate(child2, mutation_probability)
+					child1 = mutate(child1, mutation_probability, algorithm)
+					child2 = mutate(child2, mutation_probability, algorithm)
 					new_population.extend([child1, child2])
 
 			# Reemplazar la población anterior con la nueva población
 			population = new_population
 
+		# Imprimir log
+			print("Generación", generation)
+			print("Mejor individuo:", max(population, key=lambda ind: fitness_function(ind[0], ind[1])))
 	# Encontrar el individuo con el mayor valor de fitness
 	best_individual = max(population, key=lambda ind: fitness_function(ind[0], ind[1]))
+
+
 	return best_individual
 
 
@@ -77,7 +92,7 @@ def fitness_function_A(x1, x2):
 def is_valid_A(x1, x2):
     return x1 + 2 * x2 <= 30
 
-best_individual = genetic_algorithm(population_size, num_generations, mutation_probability, is_valid_A, fitness_function_A)
+best_individual = genetic_algorithm(population_size, num_generations, mutation_probability, is_valid_A, fitness_function_A, 'a')
 print("Mejor solución encontrada A:")
 print("x1 =", best_individual[0])
 print("x2 =", best_individual[1])
@@ -93,15 +108,15 @@ def fitness_function_b(x1, x2):
 def is_valid_b(x1, x2):
 		return (((3*x1) + (2*x2)) <= 18) and x1 >= 0 and x2 >= 0
 
-num_generations = 10000
+num_generations = 1000
 population_size = 100
-best_individual = genetic_algorithm(population_size, num_generations, mutation_probability, is_valid_b, fitness_function_b)
+best_individual = genetic_algorithm(population_size, num_generations, mutation_probability, is_valid_b, fitness_function_b, 'b')
 print("Mejor solución encontrada B:")
 print("x1 =", best_individual[0])
 print("x2 =", best_individual[1])
 print("Valor de la función objetivo:", fitness_function_b(best_individual[0], best_individual[1]))
 
-# # Ejercicio C
+# Ejercicio C
 # Función que queremos maximizar: 5x1-x1^2+8x2-2x2^2
 def fitness_function(x1, x2):
 		return 5*x1 - x1**2 + 8*x2 - 2*x2**2
@@ -112,7 +127,7 @@ def is_valid(x1, x2):
 
 num_generations = 100
 population_size = 100
-best_individual = genetic_algorithm(population_size, num_generations, mutation_probability, is_valid, fitness_function)
+best_individual = genetic_algorithm(population_size, num_generations, mutation_probability, is_valid, fitness_function, 'c')
 print("Mejor solución encontrada C:")
 print("x1 =", best_individual[0])
 print("x2 =", best_individual[1])
